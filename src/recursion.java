670e9037b17b2e0dfd6013b7e26fb9b0492dc336
amit <amitrajshahdaragmail.com>
2020-08-07 14:12:28 +0530

    timepass

---
+++ src/recursion.java
@@ -0,0 +1,36 @@
+public class recursion {
+    public static void main(String[] args) {
+      //  dec(5);
+       // inc(5);
+        System.out.println(fib(3));
+    }
+    public static void dec(int n)
+    {
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+    public static void inc(int n)
+    {
+        if(n==0){
+            return;
+        }
+        inc(n-1);
+        System.out.println(n);
+    }
+
+   public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+   }
+   public static int fib(int n){
+        if(n<2){
+            return n;
+        }
+        return fib(n-2)+fib(n-1);
+   }
+}
