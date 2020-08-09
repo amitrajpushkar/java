0ba481765411081218f083ffcf358a6364af6610
amit <amitrajshahdaragmail.com>
2020-08-09 16:27:08 +0530

    constructorjava

--- src/client.java
+++ src/client.java
@@ -2,7 +2,7 @@ public class client {
     public static void main(String[] args) {
         human homo=new human(23,"amit",400000);
         System.out.println(homo.age);
-
+        homo.name="jnbh";
 
     }
 }
